8012c1894d8791422fa9b312cf9c90eff7e0370a
ilGhera <andrea@ilghera.com>
2017-07-21 10:47:44 +0200

    Working on cost amount

--- includes/wcexd-single-order.php
+++ includes/wcexd-single-order.php
@@ -47,8 +47,8 @@ if(WCtoDanea::order_details($order->ID, '_order_shipping_tax') != 0) {
   $cost_vat_code = number_format(WCtoDanea::order_details($order->ID, '_order_shipping_tax') * 100 / WCtoDanea::order_details($order->ID, '_order_shipping'));
 }
 ?>
-  <CostVatCode><?php echo($cost_vat_code) ? $cost_vat_code : 'FC'; ?></CostVatCode>
-  <CostAmount><?php echo round(WCtoDanea::order_details($order->ID, '_order_shipping'), 2); ?></CostAmount>
+  <CostVatCode><?php echo($cost_vat_code) ? $cost_vat_code : ''; ?></CostVatCode>
+  <CostAmount><?php echo round(WCtoDanea::order_details($order->ID, '_order_shipping')+((WCtoDanea::order_details($order->ID, '_order_shipping')/100)*$cost_vat_code), 2); ?></CostAmount>
   <PricesIncludeVat>false</PricesIncludeVat>
   <PaymentName><?php echo WCtoDanea::order_details($order->ID, '_payment_method_title'); ?></PaymentName>
   <InternalComment><?php echo $order->post_excerpt; ?></InternalComment>
