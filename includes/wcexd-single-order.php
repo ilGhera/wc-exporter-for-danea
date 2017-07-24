1f3de1d471420998b215c0dd0d834d16d82dc54d
ilGhera <andrea@ilghera.com>
2017-07-24 23:30:31 +0200

    Restore shipping cost system

--- includes/wcexd-single-order.php
+++ includes/wcexd-single-order.php
@@ -48,13 +48,7 @@ if(WCtoDanea::order_details($order->ID, '_order_shipping_tax') != 0) {
 }
 ?>
   <CostVatCode><?php echo($cost_vat_code) ? $cost_vat_code : 'FC'; ?></CostVatCode>
-<?php if($cost_vat_code == 'FC') { ?>
-  <CostAmount><?php echo round(WCtoDanea::order_details($order->ID, '_order_shipping'), 2); ?></CostAmount>
-<?php } else { 
-  $divider = '1.' . $cost_vat_code;
-?>
-  <CostAmount><?php echo round((WCtoDanea::order_details($order->ID, '_order_shipping')/ $divider), 2); ?></CostAmount>
-<?php } ?>  <PricesIncludeVat>false</PricesIncludeVat>
+  <CostAmount><?php echo round(WCtoDanea::order_details($order->ID, '_order_shipping')); ?></CostAmount>
   <PaymentName><?php echo WCtoDanea::order_details($order->ID, '_payment_method_title'); ?></PaymentName>
   <InternalComment><?php echo $order->post_excerpt; ?></InternalComment>
   <CustomField2></CustomField2>
