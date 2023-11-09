7eec7c5f87f5ebed088052881e00c87d2e6af1a9
ilGhera <andrea@ilghera.com>
2023-11-09 13:28:45 +0100

    Classe funzioni plugin

--- includes/wcexd-functions.php
+++ includes/wcexd-functions.php
@@ -10,7 +10,31 @@
 
 defined( 'ABSPATH' ) || exit;
 
-class WCtoDanea {
+class WCEFD_Functions {
+
+    /**
+     * The constructor
+     *
+     * @param bool $init hooks with true.
+     *
+     * @return void
+     */
+    public function __construct( $init = false ) {
+
+        if ( $init ) {
+
+            /* Actions */
+            add_action( 'woocommerce_thankyou', array( $this, 'add_item_details' ), 10, 1 );
+
+            /* Filters */
+            add_filter( 'woocommerce_hidden_order_itemmeta', array( $this, 'hide_item_discount' ) );
+            add_filter( 'puc_manual_check_link-wc-exporter-for-danea-premium', array( $this, 'check_update' ) );
+            add_filter( 'puc_manual_check_message-wc-exporter-for-danea-premium', array( $this, 'update_message' ), 10, 2 );
+
+        }
+
+    }
+
 
 	/**
 	 * Get the product VAT value
@@ -20,7 +44,7 @@ class WCtoDanea {
      *
 	 * @return mixed
 	 */
-	public static function get_tax_rate( $product, $type = null ) {
+	public function get_tax_rate( $product, $type = null ) {
 
 		$output = 'FC';
 
@@ -80,7 +104,7 @@ class WCtoDanea {
      *
 	 * @return array
 	 */
-	public static function get_order_tax_items( $order, $shipping = false ) {
+	public function get_order_tax_items( $order, $shipping = false ) {
 
 		$output = array();
 
@@ -120,7 +144,7 @@ class WCtoDanea {
      *
      * @return array 
      */
-    public static function get_shipping_tax_rate( $order ) {
+    public function get_shipping_tax_rate( $order ) {
 
 		$output = 'FC';
 
@@ -158,7 +182,7 @@ class WCtoDanea {
      *
 	 * @return string
 	 */
-	public static function get_item_tax_rate( $order, $item ) {
+	public function get_item_tax_rate( $order, $item ) {
 
 		$output = 'FC';
 
@@ -195,7 +219,7 @@ class WCtoDanea {
      *
 	 * @return string
 	 */
-	public static function get_cost_description( $order ) {
+	public function get_cost_description( $order ) {
 
         $output = $order->get_shipping_method();
 
@@ -225,7 +249,7 @@ class WCtoDanea {
      *
 	 * @return mixed
 	 */
-	public static function sort_sub_categories( $a, $b ) {
+	public function sort_sub_categories( $a, $b ) {
 
 		if ( isset( $a->parent ) && isset( $b->parent ) ) {
 
@@ -249,7 +273,7 @@ class WCtoDanea {
      *
 	 * @return string la lista formattata per Danea Easyfatt
 	 */
-	public static function prepare_sub_categories( $child ) {
+	public function prepare_sub_categories( $child ) {
 
 		$list = array();
 
@@ -281,7 +305,7 @@ class WCtoDanea {
      *
 	 * @return string
 	 */
-	public static function get_product_category_name( $product, $is_variation = false ) {
+	public function get_product_category_name( $product, $is_variation = false ) {
 
 		$parent  = null;
 		$child   = array();
@@ -351,7 +375,7 @@ class WCtoDanea {
      *
      * @return int the author ID.
 	 */
-	public static function get_sensei_author( $product_id ) {
+	public function get_sensei_author( $product_id ) {
 
 		global $wpdb;
 
@@ -387,7 +411,7 @@ class WCtoDanea {
      *
 	 * @return string the meta_key name to use to get the data from the database 
 	 */
-	public static function get_italian_tax_fields_names( $field ) {
+	public function get_italian_tax_fields_names( $field ) {
 
 		$cf_name      = null;
 		$pi_name      = null;
@@ -467,7 +491,7 @@ class WCtoDanea {
      *
 	 * @return string
 	 */
-	public static function get_prices_col_name( $n ) {
+	public function get_prices_col_name( $n ) {
 
 		$include_tax = get_option( 'woocommerce_prices_include_tax' );
 
@@ -492,7 +516,7 @@ class WCtoDanea {
      *
 	 * @return string
 	 */
-	public static function get_product_notes( $product, $is_variation = false ) {
+	public function get_product_notes( $product, $is_variation = false ) {
 
 		if ( $is_variation ) {
 
@@ -547,207 +571,209 @@ class WCtoDanea {
 		}
 	}
 
-}
+    /**
+     * Save the discount percentage for the single product of the order.
+     *
+     * @param  int $order_id the WC order ID.
+     *
+     * @return void
+     */
+    public function add_item_details( $order_id ) {
 
+        $order     = wc_get_order( $order_id );
+        $user_data = get_userdata( $order->get_user_id() ); 
+        $user_role = isset( $user_data->roles[0] ) ? $user_data->roles[0] : null;
 
-/**
- * Hide item discount
- *
- * @param array $array the hidden order item metas.
- * @return arra the array updated
- */
-function wcexd_hide_item_discount( $array ) {
+        foreach ( $order->get_items() as $key => $item ) {
 
-	$array[] = '_wcexd_item_discount';
+            $regular_price = null;
+            $price         = null;
+            
+            if ( 'line_item' === $item['type'] ) {
 
-	return $array;
+                if ( $item->get_variation_id() ) {
 
-}
-add_filter( 'woocommerce_hidden_order_itemmeta', 'wcexd_hide_item_discount' );
+                    /*WooCommerce Role Based Price*/
+                    $wc_rbp = $item->get_meta( '_role_based_price', true );
 
+                    if ( $wc_rbp && isset( $wc_rbp[ $user_role ] ) ) {
 
-/**
- * Salva nel db la percentuale di sconto per singolo item dell'ordine
- *
- * @param  int $order_id l'id dell'ordine.
- */
-function wcifd_add_item_details( $order_id ) {
+                        $regular_price = isset( $wc_rbp[ $user_role ]['regular_price'] ) ? $wc_rbp[ $user_role ]['regular_price'] : null; 
+                        $price         = isset( $wc_rbp[ $user_role ]['selling_price'] ) ? $wc_rbp[ $user_role ]['selling_price'] : null;
 
-	$order     = wc_get_order( $order_id );
-    $user_data = get_userdata( $order->get_user_id() ); 
-    $user_role = isset( $user_data->roles[0] ) ? $user_data->roles[0] : null;
+                    } else {
 
-	foreach ( $order->get_items() as $key => $item ) {
-
-        $regular_price = null;
-        $price         = null;
-        
-		if ( 'line_item' === $item['type'] ) {
-
-			if ( $item->get_variation_id() ) {
-
-                /*WooCommerce Role Based Price*/
-                $wc_rbp = $item->get_meta( '_role_based_price', true );
-
-                if ( $wc_rbp && isset( $wc_rbp[ $user_role ] ) ) {
-
-                    $regular_price = isset( $wc_rbp[ $user_role ]['regular_price'] ) ? $wc_rbp[ $user_role ]['regular_price'] : null; 
-                    $price         = isset( $wc_rbp[ $user_role ]['selling_price'] ) ? $wc_rbp[ $user_role ]['selling_price'] : null;
+                        $regular_price = $regular_price ? $regular_price : get_post_meta( $item['variation_id'], '_regular_price', true );
+                        $price         = $price ? $price : get_post_meta( $item['variation_id'], '_price', true );
+                        
+                    }
 
                 } else {
 
-                    $regular_price = $regular_price ? $regular_price : get_post_meta( $item['variation_id'], '_regular_price', true );
-                    $price         = $price ? $price : get_post_meta( $item['variation_id'], '_price', true );
-                    
+                    /*WooCommerce Role Based Price*/
+                    $wc_rbp = $item->get_meta( '_role_based_price', true );
+
+                    if ( $wc_rbp && isset( $wc_rbp[ $user_role ] ) ) {
+
+                        $regular_price = isset( $wc_rbp[ $user_role ]['regular_price'] ) ? $wc_rbp[ $user_role ]['regular_price'] : null; 
+                        $price         = isset( $wc_rbp[ $user_role ]['selling_price'] ) ? $wc_rbp[ $user_role ]['selling_price'] : null;
+
+                    } else {
+
+                        $regular_price = $regular_price ? $regular_price : get_post_meta( $item['product_id'], '_regular_price', true );
+                        $price         = $price ? $price : get_post_meta( $item['product_id'], '_price', true );
+
+                    }
                 }
 
-			} else {
+                if ( $price && 0 < $regular_price ) {
 
-                /*WooCommerce Role Based Price*/
-                $wc_rbp = $item->get_meta( '_role_based_price', true );
+                    $math     = $price * 100 / $regular_price;
+                    $discount = number_format( ( 100 - $math ), 2, '.', '' );
 
-                if ( $wc_rbp && isset( $wc_rbp[ $user_role ] ) ) {
-
-                    $regular_price = isset( $wc_rbp[ $user_role ]['regular_price'] ) ? $wc_rbp[ $user_role ]['regular_price'] : null; 
-                    $price         = isset( $wc_rbp[ $user_role ]['selling_price'] ) ? $wc_rbp[ $user_role ]['selling_price'] : null;
-
-                } else {
-
-                    $regular_price = $regular_price ? $regular_price : get_post_meta( $item['product_id'], '_regular_price', true );
-                    $price         = $price ? $price : get_post_meta( $item['product_id'], '_price', true );
+                    wc_add_order_item_meta( $key, '_wcexd_item_discount', $discount );
 
                 }
-			}
-
-			if ( $price && 0 < $regular_price ) {
-
-				$math     = $price * 100 / $regular_price;
-				$discount = number_format( ( 100 - $math ), 2, '.', '' );
-
-				wc_add_order_item_meta( $key, '_wcexd_item_discount', $discount );
-
-			}
-		}
-	}
-}
-add_action( 'woocommerce_thankyou', 'wcifd_add_item_details', 10, 1 );
-
-
-/**
- * Random string
- *
- * @param  int $length la lunghezza della stringa richiesta.
- * @return string
- */
-function wcexd_rand_md5( $length ) {
-
-	$max = ceil( $length / 32 );
-	$random = '';
-
-	for ( $i = 0; $i < $max; $i ++ ) {
-
-		$random .= md5( microtime( true ) . mt_rand( 10000, 90000 ) );
-
-	}
-
-	return substr( $random, 0, $length );
-
-}
-
-
-/**
- * Modifico il nome del link di verifica aggiornamento
- */
-function wcexd_check_update() {
-
-	return __( 'Check for updates', 'wc-exporter-for-danea' );
-
-}
-add_filter( 'puc_manual_check_link-wc-exporter-for-danea-premium', 'wcexd_check_update' );
-
-
-/**
- * Modifico il messaggio del risultato aggiornamento
- *
- * @param  string $message il messagio per l'admin.
- * @param  string $status  presenza o meno di un aggiornamento disponibile, errore.
- * @return string          il messaggio restituito
- */
-function wcexd_update_message( $message = '', $status = '' ) {
-
-	if ( 'no_update' === $status ) {
-
-		$message = __( ' <strong>WooCommerce Exporter for Danea - Premium</strong> is up to date.', 'wc-exporter-for-danea' );
-
-	} else if ( 'update_available' === $status ) {
-
-		$message = __( 'A new version of <strong>WooCommerce Exporter for Danea - Premium</strong> is available.', 'wc-exporter-for-danea' );
-
-	} else {
-
-		$message = __( 'An error occurred, please try again later.', 'wc-exporter-for-danea' );
-
-	}
-
-	return $message;
-
-}
-add_filter( 'puc_manual_check_message-wc-exporter-for-danea-premium', 'wcexd_update_message', 10, 2 );
-
-
-/**
- * Listino Danea per prezzo base livello utente
- *
- * @param string $billing_email the customer email.
- *
- * @return string
- */
-function get_the_price_list( $billing_email ) {
-
-    /* Il listino di default per il prezzo base */
-    $regular_price_list = get_option( 'wcifd-regular-price-list' );
-
-    /* WooCommerce Role Based Price */
-	$wc_rbp = function_exists( 'get_wc_rbp' ) ? get_wc_rbp() : null;
-
-    /* Il plugin RBP non è installato */
-    if ( ! $wc_rbp ) {
-
-        $output = $regular_price_list;
-
-    } elseif ( is_array( $wc_rbp ) ) {
-
-        /* Recupero l'utente se registrato */
-        $user = get_user_by( 'email', $billing_email );
-
-        if ( is_object( $user ) && ! is_wp_error( $user ) ) {
-
-            /* Ruolo utente */
-            $role = isset( $user->roles[0] ) ? $user->roles[0] : null;
-
-        } else {
-
-            /* Ruolo utente */
-            $role = 'logedout';
-
+            }
         }
+    }
+    
 
-        if ( $role ) {
+    /**
+     * Hide item discount
+     *
+     * @param array $array the hidden order item metas.
+     *
+     * @return arra
+     */
+    public function hide_item_discount( $array ) {
 
-            if ( array_key_exists( $role, $wc_rbp ) ) {
+        $array[] = '_wcexd_item_discount';
 
-                /* Il listino di default previsto per questo livello utente */
-                $result = isset( $wc_rbp[ $role ]['regular_price'] ) ? $wc_rbp[ $role ]['regular_price'] : $regular_price_list;
-                $output = $result ? $result : $regular_price_list;
+        return $array;
+
+    }
+
+
+    /**
+     * Get the Danea price list to be used as base price for the specific user level.
+     *
+     * @param string $billing_email the customer email.
+     *
+     * @return string
+     */
+    public function get_the_price_list( $billing_email ) {
+
+        /* Il listino di default per il prezzo base */
+        $regular_price_list = get_option( 'wcifd-regular-price-list' );
+
+        /* WooCommerce Role Based Price */
+        $wc_rbp = function_exists( 'get_wc_rbp' ) ? get_wc_rbp() : null;
+
+        /* Il plugin RBP non è installato */
+        if ( ! $wc_rbp ) {
+
+            $output = $regular_price_list;
+
+        } elseif ( is_array( $wc_rbp ) ) {
+
+            /* Recupero l'utente se registrato */
+            $user = get_user_by( 'email', $billing_email );
+
+            if ( is_object( $user ) && ! is_wp_error( $user ) ) {
+
+                /* Ruolo utente */
+                $role = isset( $user->roles[0] ) ? $user->roles[0] : null;
 
             } else {
 
-                $output = $regular_price_list;
+                /* Ruolo utente */
+                $role = 'logedout';
+
             }
+
+            if ( $role ) {
+
+                if ( array_key_exists( $role, $wc_rbp ) ) {
+
+                    /* Il listino di default previsto per questo livello utente */
+                    $result = isset( $wc_rbp[ $role ]['regular_price'] ) ? $wc_rbp[ $role ]['regular_price'] : $regular_price_list;
+                    $output = $result ? $result : $regular_price_list;
+
+                } else {
+
+                    $output = $regular_price_list;
+                }
+
+            }
+
+            return ( sprintf( 'Listino %s', intval( $output ) ) );
 
         }
 
-        return ( sprintf( 'Listino %s', intval( $output ) ) );
+    }
+
+
+    /**
+     * Random string
+     *
+     * @param  int $length the string length.
+     *
+     * @return string
+     */
+    public function rand_md5( $length ) {
+
+        $max    = ceil( $length / 32 );
+        $random = '';
+
+        for ( $i = 0; $i < $max; $i ++ ) {
+
+            $random .= md5( microtime( true ) . mt_rand( 10000, 90000 ) );
+
+        }
+
+        return substr( $random, 0, $length );
+
+    }
+
+
+    /**
+     * Custom text for the search update link
+     *
+     * @return string
+     */
+    public function check_update() {
+
+        return __( 'Check for updates', 'wc-exporter-for-danea' );
+
+    }
+
+
+    /**
+     * Custom messages for the update result
+     *
+     * @param  string $message the message for the admin. 
+     * @param  string $status  update available or error. 
+     *
+     * @return string
+     */
+    public function update_message( $message = null, $status = null ) {
+
+        if ( 'no_update' === $status ) {
+
+            $message = __( ' <strong>WooCommerce Exporter for Danea - Premium</strong> is up to date.', 'wc-exporter-for-danea' );
+
+        } else if ( 'update_available' === $status ) {
+
+            $message = __( 'A new version of <strong>WooCommerce Exporter for Danea - Premium</strong> is available.', 'wc-exporter-for-danea' );
+
+        } else {
+
+            $message = __( 'An error occurred, please try again later.', 'wc-exporter-for-danea' );
+
+        }
+
+        return $message;
 
     }
 
