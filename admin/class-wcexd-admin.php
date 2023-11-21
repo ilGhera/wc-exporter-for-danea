3eac7a6bf3fc46d4b44d8c4c8537b51c16f9712b
ilGhera <andrea@ilghera.com>
2023-11-21 08:52:34 +0100

    Coding standard classe admin Coding standard template clienti Coding standard template ordini

--- admin/class-wcexd-admin.php
+++ admin/class-wcexd-admin.php
@@ -131,8 +131,8 @@ class WCEXD_Admin {
 			/* Header */
 			echo '<h1 class="wcexd main">' . esc_html__( 'Woocommmerce Exporter per Danea', 'wc-exporter-for-danea' ) . '</h1>';
 
-            /* The premium key form */
-            $this->premium_key_form();
+			/* The premium key form */
+			$this->premium_key_form();
 
 		echo '</div>';
 
@@ -146,34 +146,34 @@ class WCEXD_Admin {
 	}
 
 
-    /**
-     * The premium key form
-     *
-     * @return void
-     */
-    public function premium_key_form() {
+	/**
+	 * The premium key form
+	 *
+	 * @return void
+	 */
+	public function premium_key_form() {
 
-        /*Plugin premium key*/
-        $key = sanitize_text_field( get_option( 'wcexd-premium-key' ) );
+		/*Plugin premium key*/
+		$key = sanitize_text_field( get_option( 'wcexd-premium-key' ) );
 
 		if ( isset( $_POST['wcexd-premium-key'], $_POST['wcexd-premium-key-nonce'] ) && wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['wcexd-premium-key-nonce'] ) ), 'wcexd-premium-key' ) ) {
 			$key = sanitize_text_field( wp_unslash( $_POST['wcexd-premium-key'] ) );
 			update_option( 'wcexd-premium-key', $key );
 		}
 
-        echo '<form id="wcexd-options" method="post" action="">';
+		echo '<form id="wcexd-options" method="post" action="">';
 
-            echo '<label>' . esc_html__( 'Premium Key', 'wc-exporter-for-danea' ) . '</label>';
-            echo '<input type="text" class="regular-text code" name="wcexd-premium-key" id="wcexd-premium-key" placeholder="' . esc_html__( 'Add your Premium Key', 'wc-exporter-for-danea' ) . '" value="' . esc_attr( $key ) . '" />';
-            echo '<p class="description">' . esc_html__( 'Add here the Premium Key that you received by email, you will keep your copy of <strong>WooCommerce Exporter for Danea - Premium</strong> updated.', 'wc-exporter-for-danea' ) . '</p>';
-            echo '<input type="hidden" name="done" value="1" />';
+			echo '<label>' . esc_html__( 'Premium Key', 'wc-exporter-for-danea' ) . '</label>';
+			echo '<input type="text" class="regular-text code" name="wcexd-premium-key" id="wcexd-premium-key" placeholder="' . esc_html__( 'Add your Premium Key', 'wc-exporter-for-danea' ) . '" value="' . esc_attr( $key ) . '" />';
+			echo '<p class="description">' . esc_html__( 'Add here the Premium Key that you received by email, you will keep your copy of <strong>WooCommerce Exporter for Danea - Premium</strong> updated.', 'wc-exporter-for-danea' ) . '</p>';
+			echo '<input type="hidden" name="done" value="1" />';
 
-            wp_nonce_field( 'wcexd-premium-key', 'wcexd-premium-key-nonce' );
+			wp_nonce_field( 'wcexd-premium-key', 'wcexd-premium-key-nonce' );
 
-            echo '<input type="submit" class="button button-primary" value="' . esc_html__( 'Save', 'wc-exporter-for-danea' ) . '" />';
-        echo '</form>';
+			echo '<input type="submit" class="button button-primary" value="' . esc_html__( 'Save', 'wc-exporter-for-danea' ) . '" />';
+		echo '</form>';
 
-    }
+	}
 
 
 	/**
--- admin/wcexd-orders-template.php
+++ admin/wcexd-orders-template.php
@@ -11,121 +11,114 @@
 
 <div id="wcexd-ordini" class="wcexd-admin">
 
-    <h3 class="wcexd"><?php _e( 'Export your WooCommerce orders list.', 'wc-exporter-for-danea' ); ?></h3>
+	<h3 class="wcexd"><?php esc_html_e( 'Export your WooCommerce orders list.', 'wc-exporter-for-danea' ); ?></h3>
 
-    <p>
-        <?php _e( 'The import of orders in Danea is done by using a XML file.', 'wc-exporter-for-danea' ); ?>
-        <ul class="wcexd">
-            <li>'<?php _e( 'Copy the full URL of your feed with your updated WooCommerce orders list', 'wc-exporter-for-danea' ); ?></li>
-            <li>'<?php _e( 'In Danea, choose "Scarica ordini" from the menu "Strumenti/ E-commerce"', 'wc-exporter-for-danea' ); ?></li>
-            <li>'<?php _e( 'In the next window, paste the URL of your WooCommerce orders list in "Impostazioni/ indirizzo..."', 'wc-exporter-for-danea' ); ?></li>
-        <ul>
-        <p><?php _e( 'Need more information? Please, visit this page:', 'wc-exporter-for-danea' ); ?></p>
-        <a href="https://help.danea.it/easyfatt/index.htm#t=Ricezione_ordini_di_acquisto.htm" target="_blank">https://help.danea.it/easyfatt/index.htm#t=Ricezione_ordini_di_acquisto.htm</a>
-    </p>
+	<p>
+		<?php esc_html_e( 'The import of orders in Danea is done by using a XML file.', 'wc-exporter-for-danea' ); ?>
+		<ul class="wcexd">
+			<li>'<?php esc_html_e( 'Copy the full URL of your feed with your updated WooCommerce orders list', 'wc-exporter-for-danea' ); ?></li>
+			<li>'<?php esc_html_e( 'In Danea, choose "Scarica ordini" from the menu "Strumenti/ E-commerce"', 'wc-exporter-for-danea' ); ?></li>
+			<li>'<?php esc_html_e( 'In the next window, paste the URL of your WooCommerce orders list in "Impostazioni/ indirizzo..."', 'wc-exporter-for-danea' ); ?></li>
+		<ul>
+		<p><?php esc_html_e( 'Need more information? Please, visit this page:', 'wc-exporter-for-danea' ); ?></p>
+		<a href="https://help.danea.it/easyfatt/index.htm#t=Ricezione_ordini_di_acquisto.htm" target="_blank">https://help.danea.it/easyfatt/index.htm#t=Ricezione_ordini_di_acquisto.htm</a>
+	</p>
 
-    <?php
-    /* Check the admin settings */
-    $orders_statuses = get_option( 'wcexd-orders-statuses' ) ? get_option( 'wcexd-orders-statuses' ) : array( 'any' );
+	<?php
+	/* Check the admin settings */
+	$orders_statuses         = get_option( 'wcexd-orders-statuses' ) ? get_option( 'wcexd-orders-statuses' ) : array( 'any' );
+	$wcexd_orders_tax_name   = get_option( 'wcexd-orders-tax-name' );
+	$wcexd_currency_exchange = get_option( 'wcexd-currency-exchange' );
 
-    if ( isset( $_POST['wcexd-orders-sent'] ) ) {
+	if ( isset( $_POST['wcexd-orders-nonce'], $_POST['wcexd-orders-sent'] ) && wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['wcexd-orders-nonce'] ) ), 'wcexd-orders-submit' ) ) {
 
-        $orders_statuses = isset( $_POST['wcexd-orders-statuses'] ) ? map_deep( $_POST['wcexd-orders-statuses'], 'sanitize_text_field' ) : array( 'any' );
+		$orders_statuses = isset( $_POST['wcexd-orders-statuses'] ) ? map_deep( wp_unslash( $_POST['wcexd-orders-statuses'] ), 'sanitize_text_field' ) : array( 'any' );
+		update_option( 'wcexd-orders-statuses', $orders_statuses );
 
-        update_option( 'wcexd-orders-statuses', $orders_statuses );
+		$wcexd_orders_tax_name = isset( $_POST['wcexd-orders-tax-name'] ) ? sanitize_text_field( wp_unslash( $_POST['wcexd-orders-tax-name'] ) ) : 0;
+		update_option( 'wcexd-orders-tax-name', $wcexd_orders_tax_name );
 
-    }
+		$wcexd_currency_exchange = isset( $_POST['wcexd-currency-exchange'] ) ? sanitize_text_field( wp_unslash( $_POST['wcexd-currency-exchange'] ) ) : 0;
+		update_option( 'wcexd-currency-exchange', $wcexd_currency_exchange );
 
-    $wcexd_orders_tax_name = get_option( 'wcexd-orders-tax-name' );
+	}
+	?>
 
-    if ( isset( $_POST['wcexd-orders-sent'] ) ) {
+	<form name="wcexd-orders" id="wcexd-orders" class="wcexd-form" method="post" action="">
+		<table class="form-table">
+			<?php
+			$premium_key = strtolower( get_option( 'wcexd-premium-key' ) );
+			$url_code    = get_option( 'wcexd-url-code' );
 
-        $wcexd_orders_tax_name = isset( $_POST['wcexd-orders-tax-name'] ) ? sanitize_text_field( wp_unslash( $_POST['wcexd-orders-tax-name'] ) ) : 0;
-        update_option( 'wcexd-orders-tax-name', $wcexd_orders_tax_name );
+			if ( ! $url_code ) {
 
-    }
+				$url_code = wcexd_rand_md5( 6 );
+				add_option( 'wcexd-url-code', $url_code );
 
-    $wcexd_currency_exchange = get_option( 'wcexd-currency-exchange' );
+			}
 
-    if ( isset( $_POST['wcexd-orders-sent'] ) ) {
+			$receive_orders_url = __( 'Please insert your <strong>Premium Key</strong>', 'wc-exporter-for-danea' );
 
-        $wcexd_currency_exchange = isset( $_POST['wcexd-currency-exchange'] ) ? sanitize_text_field( wp_unslash( $_POST['wcexd-currency-exchange'] ) ) : 0;
-        update_option( 'wcexd-currency-exchange', $wcexd_currency_exchange );
+			if ( $premium_key ) {
 
-    }
-    ?>
-    
-    <form name="wcexd-orders" id="wcexd-orders" class="wcexd-form" method="post" action="">
-        <table class="form-table">
-            <?php
-            $premium_key = strtolower( get_option( 'wcexd-premium-key' ) );
-            $url_code    = get_option( 'wcexd-url-code' );
+				$receive_orders_url = sprintf( '%1$s/%2$s%3$s', home_url(), $premium_key, $url_code );
 
-            if ( ! $url_code ) {
+			}
+			?>
+			<tr>
+				<th scope="row"><?php esc_html_e( 'Orders status', 'wc-exporter-for-danea' ); ?></th>
+				<td>
+					<select form="wcexd-orders" class="wcexd" name="wcexd-orders-statuses[]" multiple data-placeholder="<?php esc_html_e( 'All orders', 'wc-exporter-for-danea' ); ?>">
+						<?php
+						$statuses = wc_get_order_statuses();
 
-                $url_code = wcexd_rand_md5( 6 );
-                add_option( 'wcexd-url-code', $url_code );
+						foreach ( $statuses as $key => $value ) {
 
-            }
+							echo '<option name="' . esc_attr( $key ) . '" value="' . esc_attr( $key ) . '"';
+							echo ( in_array( $key, $orders_statuses, true ) ) ? ' selected="selected">' : '>';
+							echo esc_html__( $value, 'wc-exporter-for-danea' );
+							echo '</option>';
 
-            $receive_orders_url = __( 'Please insert your <strong>Premium Key</strong>', 'wc-exporter-for-danea' );
+						}
+						?>
+					</select>
+					<p class="description"><?php esc_html_e( 'Select the order\'s status that you want to import in Danea', 'wc-exporter-for-danea' ); ?></p>
+				</td>
+			</tr>
+			<tr>
+				<th scope="row"><?php esc_html_e( 'Tax name', 'wc-exporter-for-danea' ); ?></th>
+				<td>
+					<label for="wcexd-orders-tax-name">
+						<input type="checkbox" name="wcexd-orders-tax-name" value="1"<?php echo 1 === intval( $wcexd_orders_tax_name ) ? ' checked="checked"' : ''; ?>>
+						<?php esc_html_e( 'Export the tax name instead of the rate', 'wc-exporter-for-danea' ); ?>
+					</label>
+					<p class="description"><?php esc_html_e( 'Recommended option if the tax rates were imported from Danea Easyfatt previously.', 'wc-exporter-for-danea' ); ?></p>
+				</td>
+			</tr>
+			<tr>
+				<th scope="row"><?php esc_html_e( 'Currency Exchange', 'wc-exporter-for-danea' ); ?></th>
+				<td>
+					<label for="wcexd-currency-exchange">
+						<input type="checkbox" name="wcexd-currency-exchange" value="1"<?php echo 1 === $wcexd_currency_exchange ? ' checked="checked"' : ''; ?>>
+						<?php esc_html_e( 'Export orders in euros', 'wc-exporter-for-danea' ); ?>
+					</label>
+					<p class="description"><?php esc_html_e( 'Export orders received in dollars into euros using the most recent exchange rate from the Bank of Italy.', 'wc-exporter-for-danea' ); ?></p>
+				</td>
+			</tr>
+			<tr>
+				<th scope="row"><?php esc_html_e( 'Feed URL', 'wc-exporter-for-danea' ); ?></th>
+				<td>
+					<div class="wcexd-copy-url"><span<?php echo( ! $premium_key ? ' class="wcexd-red"' : null ); ?>><?php echo esc_url( $receive_orders_url ); ?></span></div>
+					<p class="description"><?php esc_html_e( 'Add this URL to the <b>Settings</b> tab of the function <b>Download orders</b> (Ctrl+O) in Danea.', 'wc-exporter-for-danea' ); ?></p>
+				</td>
+			</tr>
+		</table>                      
 
-            if ( $premium_key ) {
+		<?php wp_nonce_field( 'wcexd-orders-submit', 'wcexd-orders-nonce' ); ?>
 
-                $receive_orders_url = sprintf( '%1$s/%2$s%3$s', home_url(), $premium_key, $url_code );
-
-            }
-            ?>
-            <tr>
-                <th scope="row"><?php echo __( 'Orders status', 'wc-exporter-for-danea' ); ?></th>
-                <td>
-                    <select form="wcexd-orders" class="wcexd" name="wcexd-orders-statuses[]" multiple data-placeholder="<?php echo __( 'All orders', 'wc-exporter-for-danea' ); ?>">
-                        <?php
-                        $statuses = wc_get_order_statuses();
-
-                        foreach ( $statuses as $key => $value ) {
-
-                            echo '<option name="' . $key . '" value="' . $key . '"';
-                            echo ( in_array( $key, $orders_statuses ) ) ? ' selected="selected">' : '>';
-                            echo __( $value, 'wc-exporter-for-danea' ) . '</option>';
-
-                        }
-                        ?>
-                    </select>
-                    <p class="description"><?php echo __( 'Select the order\'s status that you want to import in Danea', 'wc-exporter-for-danea' ); ?></p>
-                </td>
-            </tr>
-            <tr>
-                <th scope="row"><?php echo __( 'Tax name', 'wc-exporter-for-danea' ); ?></th>
-                <td>
-                    <label for="wcexd-orders-tax-name">
-                        <input type="checkbox" name="wcexd-orders-tax-name" value="1"<?php echo 1 === intval( $wcexd_orders_tax_name ) ? ' checked="checked"' : ''; ?>>
-                        <?php echo __( 'Export the tax name instead of the rate', 'wc-exporter-for-danea' ); ?>
-                    </label>
-                    <p class="description"><?php echo __( 'Recommended option if the tax rates were imported from Danea Easyfatt previously.', 'wc-exporter-for-danea' ); ?></p>
-                </td>
-            </tr>
-            <tr>
-                <th scope="row"><?php echo __( 'Currency Exchange', 'wc-exporter-for-danea' ); ?></th>
-                <td>
-                    <label for="wcexd-currency-exchange">
-                        <input type="checkbox" name="wcexd-currency-exchange" value="1"<?php echo 1 === $wcexd_currency_exchange ? ' checked="checked"' : ''; ?>>
-                        <?php echo __( 'Export orders in euros', 'wc-exporter-for-danea' ); ?>
-                    </label>
-                    <p class="description"><?php echo __( 'Export orders received in dollars into euros using the most recent exchange rate from the Bank of Italy.', 'wc-exporter-for-danea' ); ?></p>
-                </td>
-            </tr>
-            <tr>
-                <th scope="row"><?php echo __( 'Feed URL', 'wc-exporter-for-danea' ); ?></th>
-                <td>
-                    <div class="wcexd-copy-url"><span<?php echo( ! $premium_key ? ' class="wcexd-red"' : null ); ?>><?php echo $receive_orders_url; ?></span></div>
-                    <p class="description"><?php echo __( 'Add this URL to the <b>Settings</b> tab of the function <b>Download orders</b> (Ctrl+O) in Danea.', 'wc-exporter-for-danea' ); ?></p>
-                </td>
-            </tr>
-        </table>                      
-        <p class="submit">
-            <input type="hidden" name="wcexd-orders-sent" value="1">
-            <input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e( 'Save options', 'wc-exporter-for-danea' ); ?>" />
-        </p>	    
-    </form>
+		<p class="submit">
+			<input type="hidden" name="wcexd-orders-sent" value="1">
+			<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e( 'Save options', 'wc-exporter-for-danea' ); ?>" />
+		</p>	    
+	</form>
 </div>
