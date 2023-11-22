9844ec674700f82d64e0e8cad518bfb2b6707436
ilGhera <andrea@ilghera.com>
2023-11-22 10:22:08 +0100

    Coding standard in template ordini

--- admin/wcexd-orders-template.php
+++ admin/wcexd-orders-template.php
@@ -24,59 +24,59 @@
 		<a href="https://help.danea.it/easyfatt/index.htm#t=Ricezione_ordini_di_acquisto.htm" target="_blank">https://help.danea.it/easyfatt/index.htm#t=Ricezione_ordini_di_acquisto.htm</a>
 	</p>
 
-    <form name="wcexd-orders" id="wcexd-orders" class="wcexd-form" method="post" action="">
-        <table class="form-table">
-            <?php $receive_orders_url = __( 'Please insert your <strong>Premium Key</strong>', 'wc-exporter-for-danea' ); ?>
-            <tr>
-                <th scope="row"><?php echo __( 'Orders status', 'wc-exporter-for-danea' ); ?></th>
-                <td>
-                    <select form="wcexd-orders" class="wcexd" name="wcexd-orders-statuses[]" multiple data-placeholder="<?php echo __( 'All orders', 'wc-exporter-for-danea' ); ?>">
-                        <?php
-                        $statuses = wc_get_order_statuses();
+	<form name="wcexd-orders" id="wcexd-orders" class="wcexd-form" method="post" action="">
+		<table class="form-table">
+			<?php $receive_orders_url = __( 'Please insert your <strong>Premium Key</strong>', 'wc-exporter-for-danea' ); ?>
+			<tr>
+				<th scope="row"><?php esc_html_e( 'Orders status', 'wc-exporter-for-danea' ); ?></th>
+				<td>
+					<select form="wcexd-orders" class="wcexd" name="wcexd-orders-statuses[]" multiple data-placeholder="<?php esc_html_e( 'All orders', 'wc-exporter-for-danea' ); ?>">
+						<?php
+						$statuses = wc_get_order_statuses();
 
-                        foreach ( $statuses as $key => $value ) {
+						foreach ( $statuses as $key => $value ) {
 
-                            echo '<option name="' . $key . '" value="' . $key . '"';
-                            echo __( $value, 'wc-exporter-for-danea' ) . '</option>';
+							echo '<option name="' . esc_attr( $key ) . '" value="' . esc_attr( $key ) . '"';
+							esc_html_e( $value, 'wc-exporter-for-danea' ) . '</option>';
 
-                        }
-                        ?>
-                    </select>
-                    <p class="description"><?php echo __( 'Select the order\'s status that you want to import in Danea', 'wc-exporter-for-danea' ); ?></p>
-                </td>
-            </tr>
-            <tr>
-                <th scope="row"><?php echo __( 'Tax name', 'wc-exporter-for-danea' ); ?></th>
-                <td>
-                    <label for="wcexd-orders-tax-name">
-                        <input type="checkbox" name="wcexd-orders-tax-name" value="1" disabled="disabled">
-                        <?php echo __( 'Export the tax name instead of the rate', 'wc-exporter-for-danea' ); ?>
-                    </label>
-                    <p class="description"><?php echo __( 'Recommended option if the tax rates were imported from Danea Easyfatt previously.', 'wc-exporter-for-danea' ); ?></p>
-                </td>
-            </tr>
-            <tr>
-                <th scope="row"><?php echo __( 'Currency Exchange', 'wc-exporter-for-danea' ); ?></th>
-                <td>
-                    <label for="wcexd-currency-exchange">
-                        <input type="checkbox" name="wcexd-currency-exchange" value="1" disabled="disabled">
-                        <?php echo __( 'Export orders in euros', 'wc-exporter-for-danea' ); ?>
-                    </label>
-                    <p class="description"><?php echo __( 'Export orders received in dollars into euros using the most recent exchange rate from the Bank of Italy.', 'wc-exporter-for-danea' ); ?></p>
-                </td>
-            </tr>
-            <tr>
-                <th scope="row"><?php echo __( 'Feed URL', 'wc-exporter-for-danea' ); ?></th>
-                <td>
-                    <div class="wcexd-copy-url"><span class="wcexd-red"><?php echo $receive_orders_url; ?></span></div>
-                    <p class="description"><?php echo __( 'Add this URL to the <b>Settings</b> tab of the function <b>Download orders</b> (Ctrl+O) in Danea.', 'wc-exporter-for-danea' ); ?></p>
-                    <?php WCEXD_Admin::go_premium(); ?>
-                </td>
-            </tr>
-        </table>                      
-        <p class="submit">
-            <input type="hidden" name="wcexd-orders-sent" value="1">
-            <input type="submit" name="Submit" class="button-primary" disabled="disabled" value="<?php esc_attr_e( 'Save options', 'wc-exporter-for-danea' ); ?>" />
-        </p>	    
-    </form>
+						}
+						?>
+					</select>
+					<p class="description"><?php esc_html_e( 'Select the order\'s status that you want to import in Danea', 'wc-exporter-for-danea' ); ?></p>
+				</td>
+			</tr>
+			<tr>
+				<th scope="row"><?php esc_html_e( 'Tax name', 'wc-exporter-for-danea' ); ?></th>
+				<td>
+					<label for="wcexd-orders-tax-name">
+						<input type="checkbox" name="wcexd-orders-tax-name" value="1" disabled="disabled">
+						<?php esc_html_e( 'Export the tax name instead of the rate', 'wc-exporter-for-danea' ); ?>
+					</label>
+					<p class="description"><?php esc_html_e( 'Recommended option if the tax rates were imported from Danea Easyfatt previously.', 'wc-exporter-for-danea' ); ?></p>
+				</td>
+			</tr>
+			<tr>
+				<th scope="row"><?php esc_html_e( 'Currency Exchange', 'wc-exporter-for-danea' ); ?></th>
+				<td>
+					<label for="wcexd-currency-exchange">
+						<input type="checkbox" name="wcexd-currency-exchange" value="1" disabled="disabled">
+						<?php esc_html_e( 'Export orders in euros', 'wc-exporter-for-danea' ); ?>
+					</label>
+					<p class="description"><?php esc_html_e( 'Export orders received in dollars into euros using the most recent exchange rate from the Bank of Italy.', 'wc-exporter-for-danea' ); ?></p>
+				</td>
+			</tr>
+			<tr>
+				<th scope="row"><?php esc_html_e( 'Feed URL', 'wc-exporter-for-danea' ); ?></th>
+				<td>
+					<div class="wcexd-copy-url"><span class="wcexd-red"><?php esc_url_e( $receive_orders_url ); ?></span></div>
+					<p class="description"><?php esc_html_e( 'Add this URL to the <b>Settings</b> tab of the function <b>Download orders</b> (Ctrl+O) in Danea.', 'wc-exporter-for-danea' ); ?></p>
+					<?php WCEXD_Admin::go_premium(); ?>
+				</td>
+			</tr>
+		</table>                      
+		<p class="submit">
+			<input type="hidden" name="wcexd-orders-sent" value="1">
+			<input type="submit" name="Submit" class="button-primary" disabled="disabled" value="<?php esc_attr_e( 'Save options', 'wc-exporter-for-danea' ); ?>" />
+		</p>	    
+	</form>
 </div>
